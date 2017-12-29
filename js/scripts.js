57d4f5cf8fe4c1405acf7f5dda3a3217986f301d
Piotr Sociński <psocinski@gmail.com>
2017-12-29 15:59:43 +0100

    Add project

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var amount = document.getElementsByClassName('button'),
+	button = amount.length;
+	
+console.log(button);
+
+for (var i = 0; i < button; i++) {
+	
+	alert(button.innerHTML = console.log(button));
+
+}
